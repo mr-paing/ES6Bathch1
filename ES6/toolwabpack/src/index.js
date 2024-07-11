174080bc9cd3b442f92f0b576b53cfb18eecdadb
Mr-Paing <tunhan972@gmail.com>
2024-07-11 17:19:57 +0630

    11july2024_5:19PM_ES6

---
+++ ES6/toolwabpack/src/index.js
@@ -0,0 +1,16 @@
+import {Phone} from "./phone.js";
+
+const obj1 = new Phone("IPhone","5G");
+        console.log(obj1);
+
+        console.log(obj1.getbrandname());
+        console.log(obj1.getphonenetwork());
+        console.log(obj1.getcolor());
+
+
+        // calling outside 
+        // console.log(_brand.get(obj1)); // IPhone
+        // console.log(_network.get(obj1)); // 5G
+        // console.log(_color.get(obj1)()); // Phone {}
+
+        console.log('Hello Indo');
